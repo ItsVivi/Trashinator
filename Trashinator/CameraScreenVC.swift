a51f35813408205a6c1a6bb56d0df0065e9f9026
Jacob <jacobrgrowan@gmail.com>
2019-02-02 18:32:45 -0400

    yay

--- Trashinator/CameraScreenVC.swift
+++ Trashinator/CameraScreenVC.swift
@@ -8,11 +8,245 @@
 
 import UIKit
 import CoreML
+import Foundation
 
 class CameraScreenVC: UIViewController, UINavigationControllerDelegate {
 
-    // let blue = Array(contentsOfFile: "blueItems.txt", encoding: String.Encoding.utf8, error: nil)
-
+    let blueList =  ["aerosolCan",
+                     "aluminum",
+                     "bottle",
+                     "steel can",
+                     "tin can",
+                     "aluminum can",
+                     "container",
+                     "pot",
+                     "pan",
+                     "utensil",
+                     "book",
+                     "bristol board",
+                     "boxboard box",
+                     "cardboard",
+                     "cards catalogue",
+                     "cereal box",
+                     "coffee cup",
+                     "drink tray",
+                     "egg carton",
+                     "envelope",
+                     "fast food bag",
+                     "paper bag",
+                     "fast food box",
+                     "pizza box",
+                     "file folder",
+                     "flyer",
+                     "newspaper",
+                     "magazine",
+                     "phone book",
+                     "plate",
+                     "cup",
+                     "poster",
+                     "sticky note",
+                     "wrapping paper bottle",
+                     "container",
+                     "cupsand dish",
+                     "hard plastic packaging",
+                     "jug",
+                     "medicine bottle",
+                     "milk jug",
+                     "juice jug",
+                     "frozen juice can",
+                     "lid",
+                     "cover",
+                     "milk carton",
+                     "juice carton",
+                     "styrofoam",
+                     "electronic",
+                     "plastic bag",
+                     "water bottle"]
+    
+    let clearList = ["balloon",
+                     "binder",
+                     "board game",
+                     "candle",
+                     "carbon paper",
+                     "cat litter",
+                     "chalk",
+                     "cigarette butt",
+                     "clothes",
+                     "linen sheet",
+                     "dirt",
+                     "dust",
+                     "disinfecting wipe",
+                     "dryer lint",
+                     "sheet",
+                     "elastic band",
+                     "fabric",
+                     "footwear",
+                     "furnace",
+                     "filter",
+                     "latex glove",
+                     "rubber glove",
+                     "leather",
+                     "light bulb",
+                     "marker",
+                     "crayon",
+                     "mirror",
+                     "nylon",
+                     "pencil",
+                     "pen",
+                     "pet training waste pad",
+                     "pet waste",
+                     "photo",
+                     "picture frame",
+                     "pillow",
+                     "pillow case",
+                     "pottery",
+                     "pyrex",
+                     "rubber",
+                     "sandpaper",
+                     "silica gel pack",
+                     "sports equipment",
+                     "sticker",
+                     "string",
+                     "tape",
+                     "toys",
+                     "transparency",
+                     "vacuum bag",
+                     "vase",
+                     "wallpaper",
+                     "yarn",
+                     "baby wipe",
+                     "bandage",
+                     "gauze",
+                     "brush",
+                     "comb",
+                     "colostomy tube feeding bag",
+                     "condom",
+                     "cosmetics",
+                     "cosmetic wipe",
+                     "cotton ball",
+                     "dental floss",
+                     "diaper",
+                     "disposable razor",
+                     "feminine hygiene product",
+                     "q-tip",
+                     "toothpaste tube",
+                     "towel",
+                     "absorbent pad",
+                     "ceramic dish",
+                     "chip bag",
+                     "cork",
+                     "dish cloth",
+                     "glass",
+                     "granola bar",
+                     "candy bar wrapper",
+                     "parchment paper",
+                     "plastic cutlery",
+                     "plastic packaging",
+                     "plastic wrap",
+                     "bubble wrap",
+                     "rag",
+                     "rubber glove",
+                     "scouring pad",
+                     "sponge",
+                     "single serve coffee pod",
+                     "single-use coffee packet",
+                     "soft plastic",
+                     "straw",
+                     "toothpick",
+                     "twist tie",
+                     "water filter",
+                     "softener salt",
+                     "waxed paper wrapper"]
+    
+    let greenList = ["apple",
+                     "banana",
+                     "bone",
+                     "coffee filter",
+                     "coffee ground",
+                     "cooking grease",
+                     "dairy product",
+                     "expired food",
+                     "fish",
+                     "shell fish",
+                     "food scrap",
+                     "leftover",
+                     "fruit",
+                     "vegetable",
+                     "meat",
+                     "oil",
+                     "fat",
+                     "rotten food",
+                     "moldy food",
+                     "egg shell",
+                     "shellfish shell",
+                     "tea bag",
+                     "grass clipping",
+                     "leaf",
+                     "plant",
+                     "flower",
+                     "weed",
+                     "sawdust",
+                     "wood chips",
+                     "soil",
+                     "twig",
+                     "branch",
+                     "napkin",
+                     "paper",
+                     "towel",
+                     "tissue",
+                     "tobacco"]
+    
+    let hazardList = ["acid",
+                      "adhesive",
+                      "glue",
+                      "aerosol",
+                      "antifreeze",
+                      "coolant",
+                      "glycol",
+                      "battery",
+                      "car battery",
+                      "household bleach",
+                      "butane cartridge",
+                      "car care product",
+                      "caulking",
+                      "chemical",
+                      "cleaning product",
+                      "computer monitor",
+                      "cooking oil",
+                      "driveway sealant",
+                      "fertilizer",
+                      "flea powder",
+                      "gasoline",
+                      "diesel fuel",
+                      "herbicide",
+                      "fungicide",
+                      "kerosene",
+                      "light bulb",
+                      "lighter",
+                      "medication",
+                      "prescription",
+                      "mercury",
+                      "thermometer",
+                      "thermostat",
+                      "methanol",
+                      "mineral spirit",
+                      "motor oil",
+                      "filter",
+                      "nail polish",
+                      "remover oven cleaner",
+                      "paint",
+                      "pesticide",
+                      "pool chemical",
+                      "polish and wax",
+                      "propane tank",
+                      "propane cylinder",
+                      "rubbing alcohol",
+                      "rust remover",
+                      "solvent",
+                      "stain",
+                      "varnish"]
+    
+    
     // MARK: IB Outlets
     @IBOutlet weak var itemNameLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
@@ -49,6 +283,7 @@ class CameraScreenVC: UIViewController, UINavigationControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
 }
 
@@ -101,6 +336,26 @@ extension CameraScreenVC: UIImagePickerControllerDelegate {
             return
         }
         itemNameLabel.text = "\(prediction.classLabel)"
+        let accuracy = "\(prediction.classLabelProbs)"
+        print("The likelyhood of this being accurate is: \(accuracy)%")
+
+        if blueList.contains(String(prediction.classLabel)) {
+            print("blue match")
+            itemNameLabel.text = "\(prediction.classLabel)s go in the blue garbage"
+        } else if greenList.contains(String(prediction.classLabel)) {
+            print("green match")
+            itemNameLabel.text = "\(prediction.classLabel)s go in the green garbage"
+        } else if clearList.contains(String(prediction.classLabel)) {
+            print("clear match")
+            itemNameLabel.text = "\(prediction.classLabel)s go in the clear garbage"
+        } else if hazardList.contains(String(prediction.classLabel)) {
+            print("hazard match")
+            itemNameLabel.text = "\(prediction.classLabel)s go in the hazardous disposal"
+        } else {
+            print("no match")
+            itemNameLabel.text = "\(prediction.classLabel) does not match any items on the list"
+        }
+        
     }
 }
 
