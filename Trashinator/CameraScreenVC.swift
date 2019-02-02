f754b0f7e7baabebd37d75f32edebae20c7b00e8
Jacob <jacobrgrowan@gmail.com>
2019-02-02 15:50:53 -0400

    stuff and things

--- Trashinator/CameraScreenVC.swift
+++ Trashinator/CameraScreenVC.swift
@@ -11,6 +11,9 @@ import CoreML
 
 class CameraScreenVC: UIViewController, UINavigationControllerDelegate {
 
+    // let text = Array(contentsOfFile: blueItems.txt, encoding: NSUTF8StringEncoding, error: nil)
+
+    
     // MARK: IB Outlets
     @IBOutlet weak var itemNameLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
