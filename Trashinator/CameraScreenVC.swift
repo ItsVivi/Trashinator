586db77df6f8c78acf264a7ab45b4f6d3d568f38
Jacob <jacobrgrowan@gmail.com>
2019-02-02 12:33:34 -0400

    Added ML model and some camera/image pickers

--- Trashinator/CameraScreenVC.swift
+++ Trashinator/CameraScreenVC.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
-class CameraScreenVC: UIViewController {
+class CameraScreenVC: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     // MARK: IB Outlets
     @IBOutlet weak var itemNameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var cameraScreenNavBar: UINavigationItem!
+    
+    @IBAction func cameraNavBarButton(_ sender: UIBarButtonItem) {
+        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+            return
+        }
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self
+        cameraPicker.sourceType = .camera
+        cameraPicker.allowsEditing = false
+        
+        present(cameraPicker, animated: true)
+    }
+    @IBAction func libraryNavBarButton(_ sender: UIBarButtonItem) {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = false
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        
+        present(picker, animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         initLabels()
@@ -23,7 +49,6 @@ class CameraScreenVC: UIViewController {
     func initLabels() {
         itemNameLabel.text = "placeholder"
         itemNameLabel.font = UIFont.systemFont(ofSize: 44.0)
-        cameraScreenNavBar.title = "zach was here"
+        cameraScreenNavBar.title = "Trashinator"
     }
 }
-
