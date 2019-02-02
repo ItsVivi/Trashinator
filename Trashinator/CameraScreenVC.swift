40d756d83c25d57ae08078aed83c7cedbd336406
Jacob Rowan <jacobrgrowan@gmail.com>
2019-02-02 10:35:46 -0400

    Initial Commit

---
+++ Trashinator/CameraScreenVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Trashinator
+//
+//  Created by Jacob Rowan on 2019-02-02.
+//  Copyright © 2019 Trashinators. All rights reserved.
+//
+
+import UIKit
+
+class CameraScreenVC: UIViewController {
+
+    // MARK: IB Outlets
+    @IBOutlet weak var itemNameLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initLabel()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    // MARK: Actions
+    func initLabel() {
+        itemNameLabel.text = nil
+        
+    }
+}
+
